61b268a03b07f5d2d6e5c7f744f8e19a64c18a2d
Rabia-Rajput <49317644+Rabia-Rajput@users.noreply.github.com>
2019-04-09 16:31:00 +0500

    Add files via upload

---
+++ Lab#1/Task1.java
@@ -0,0 +1,12 @@
+class Task1{
+public static void main( String[] args){
+String name="Rabia";
+String rollno="18SW124";
+double per=95.5;
+char p='%';
+char grade='A';
+boolean status=true;
+System.out.println("Name: "+ name);
+System.out.println("Roll no: "+ rollno);
+System.out.println("Percentage: "+ per+p);
+System.out.println("Status: Pass= "+ status);}}
---
+++ Lab#1/Task2.java
@@ -0,0 +1,6 @@
+class Task2{
+public static void main( String[] args){
+double a=50.25;
+long b=(int)a;
+double c=a-b;
+System.out.println(a + " " + b +" " + c); }}
